5177ce9d777f31b0b87fd2040b4474fba454084b
Gaurav Mishra <gauravmishra2401@gmail.com>
2023-12-08 17:07:06 +0530

    added section for topics

--- common_files/innerTheme.js
+++ common_files/innerTheme.js
@@ -6,6 +6,20 @@ window.addEventListener("message", (event) => {
   }
 });
 
+// function addPreLoader(){
+//   fetch("../languages/innerPreLoader.html").then((res, err) => {
+//     if (res.ok) {
+//       res.text().then((text) => {
+//         document.body.innerHTML += `
+//         <iframe src="../languages/innerPreLoader.html" frameborder="0" title="Pre Loader" id="preloader"></iframe>
+//         `;
+//       });
+//     } else {
+//       console.log(err);
+//     }
+//   })
+// }
+// addPreLoader();
 function setTheme(themevalue) {
   console.log("called");
   // theme = localStorage.getItem("theme");
@@ -21,6 +35,48 @@ function setTheme(themevalue) {
 }
 
 window.addEventListener("DOMContentLoaded", ()=>{
+  wrapContent();
   theme = localStorage.getItem("theme");
   setTheme(theme);
+  // const preLoader = document.getElementById("preloader");
+  // preLoader.style.display = "none";
+  // test();
 });
+
+// function test(){
+//   setTimeout(()=>{
+//     const preLoader = document.getElementById("preloader");
+//     console.log("test");
+//     if(!preLoader) test();
+//     else preLoader.style.display = "none";
+//   }, 10)
+// }
+
+function wrapContent () {
+  const body = document.body.innerHTML;
+  document.body.innerHTML = `
+      <main>${body}</main>
+      <aside id="sideTopicSection">
+        <section class="top">Topics</section>
+        <section id="topics"></section>
+      </aside>
+  `;
+  getTopics();
+}
+
+function getTopics () {
+  const topics = document.querySelectorAll("h2");
+  const topicBox = document.getElementById("topics");
+  let topicData = '';
+  // console.log(topics);
+  if(topics && topics.length > 1){
+    topics.forEach(topic => {
+      topic.setAttribute("id", topic.innerText);
+      topicData += `<a href="#${topic.innerText}">${topic.innerText}</a>`;
+    });
+    topicBox.innerHTML = topicData;
+  } else {
+    const sideTopicSection = document.querySelector('#sideTopicSection');
+    sideTopicSection.style.display = "none";
+  }
+}
