e4adb116dffce5fbe3dce0f7048f8bb37f790986
Gaurav Mishra <gauravmishra2401@gmail.com>
2024-02-18 15:35:31 +0530

    Refactor header.js to dynamically generate course options

--- common_files/header.js
+++ common_files/header.js
@@ -2,6 +2,24 @@ async function getHeader() {
     const res = await fetch("../components/header.html");
     const header = await res.text();
     document.querySelector('nav').innerHTML = header;
+    setOptions();
+}
+
+async function setOptions() {
+    const res = await fetch("../courses.json");
+    const data = await res.json();
+
+    const options = document.querySelector('.options');
+    let content = "";
+
+    data.forEach(course => {
+        let name = course.name;
+        // make the first character bigger
+        name = name.charAt(0).toUpperCase() + name.slice(1);
+
+        content += `<a href="${course.url ? course.url : `outer.html?lang=${course.name}`}" lang="${course.name}">${course.title ?course.title : name}</a>`
+    })
+    options.innerHTML = content;
 }
 
 function setHamburgerListeners() {
