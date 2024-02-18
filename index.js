1cc6292b95fdd9acc5dc3bb463a2a0ebef1a0e8c
Gaurav Mishra <gauravmishra2401@gmail.com>
2024-02-18 15:21:04 +0530

    Add new courses and update course boxes

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function toggleTheme() {
   }
 }
 
-window.addEventListener("DOMContentLoaded", () => {
+function setTheme() {
   theme = localStorage.getItem("theme");
   if (!theme) {
     theme = "dark";
@@ -29,6 +29,39 @@ window.addEventListener("DOMContentLoaded", () => {
   if (theme === "light") document.body.classList.remove("dark");
   document.body.classList.add(theme);
   themeBtn.innerHTML = theme === "light" ? "🌙" : "☀️";
+}
+
+async function getData() {
+  const res = await fetch("courses.json");
+  const data = await res.json();
+  // setData(data);
+  return data;
+}
+
+function setData(data){
+  const outerBox = document.querySelector(".boxes");
+
+  let content = "";
+
+  data.forEach(course => {
+    content += `<a href="${course.url ? course.url : `languages/outer.html?lang=${course.name}`}" class="box" id="${course.name}">
+    <div class="langlogo">
+      <img src="images/${course.image}" alt="${course.name} logo">
+    </div>
+    <div class="langname">${course.name}</div>
+  </a>`
+  })
+
+  outerBox.innerHTML = content;
+}
+
+window.addEventListener("DOMContentLoaded", async () => {
+  setTheme();
+
+  const data = await getData();
+  console.log(data);
+
+  setData(data);
 });
 
 themeBtn.addEventListener("click", toggleTheme);
