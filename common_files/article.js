a8d0e58eba2e5e558f0327c11bf9874d3a7df8a4
Gaurav Mishra <gauravmishra2401@gmail.com>
2024-04-02 21:25:32 +0530

    Add setHeader function to article.js

---
+++ common_files/article.js
@@ -0,0 +1,19 @@
+async function setHeader() {
+    fetch('../components/header.html')
+    .then(response => response.text())
+    .then(data => {
+        document.querySelector('nav').innerHTML = data;
+        const languagesBox = document.querySelector('nav ul li .languages');
+        if (languagesBox) {
+            languagesBox.parentElement.removeChild(languagesBox);
+        }
+        setTheme();
+        setListenersTheme();
+    });
+}
+
+
+window.addEventListener("DOMContentLoaded", async () => {
+    // setData()
+    setHeader();
+})
