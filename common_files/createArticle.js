e55e8ed85c6f7e5cddc12a199f38b7f16b5edf2a
Gaurav Mishra <gauravmishra2401@gmail.com>
2024-04-17 03:56:54 +0530

    Add createArticle function and setListeners for form submission

--- common_files/createArticle.js
+++ common_files/createArticle.js
@@ -1,3 +1,5 @@
+const errorbox = document.querySelector('#errorbox');
+
 async function setHeader() {
     fetch('../components/header.html')
     .then(response => response.text())
@@ -12,7 +14,63 @@ async function setHeader() {
     });
 }
 
+async function createArticle(data) {
+    const api = 'http://127.0.0.1:8000/api/v1/article/create-article';
+
+    // Send request to server
+    fetch(api, {
+      method: 'POST',
+      body: data,
+      credentials: "include"
+    })
+    .then(response => {
+      if(response.ok) {
+        return response.json();
+      } else {
+        errorbox.innerHTML = data.message;
+      }
+    })
+    .then(data => {
+        if(data) {
+            errorbox.innerHTML = "Article created successfully";
+            errorbox.classList.add('success');
+
+            setTimeout(function() {
+                window.location.href = `./articles.html`;
+            }, 2000)
+        }
+    })
+    .catch(error => {
+      errorbox.innerHTML = error.message;
+    });
+}
+
+function setListeners() {
+    const form = document.querySelector('form');
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault();
+
+        const form = new FormData(signupForm);
+        const {title, content, image} = Object.fromEntries(form);
+    
+        // Validate form
+        if(
+        [title, content].some(field => !field || field === "")
+        ) {
+            errorbox.innerHTML = "All fields are required";
+            return;
+        }
+    
+        if(!image) {
+            errorbox.innerHTML = "Profile picture and cover image are required";
+            return;
+        }
+
+        await createArticle(form);
+    });
+}
+
 window.addEventListener("DOMContentLoaded", async () => {
     setHeader();
-    // setListeners();
+    setListeners();
 })
