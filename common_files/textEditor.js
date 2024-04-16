84817146f1188d37788f4d264f9b699253939d2e
Gaurav Mishra <gauravmishra2401@gmail.com>
2024-04-16 16:35:46 +0530

    Removed createArticle.css and createArticle.js, Add textEditor.css and textEditor.js

---
+++ common_files/textEditor.js
@@ -0,0 +1,43 @@
+// const editor = new FroalaEditor('#editor')
+const quill = new Quill('#editor', {
+    theme: 'snow'
+  });
+const copybtn = document.querySelector('#copy');
+const nextbtn = document.querySelector('#next');
+
+async function setHeader() {
+    fetch('../components/header.html')
+    .then(response => response.text())
+    .then(data => {
+        document.querySelector('nav').innerHTML = data;
+        const languagesBox = document.querySelector('nav ul li .languages');
+        if (languagesBox) {
+            languagesBox.parentElement.removeChild(languagesBox);
+        }
+        setTheme();
+        setListenersTheme();
+    });
+}
+
+function setListeners() {
+    copybtn.addEventListener('click', () => {
+        const content = quill.root.innerHTML;
+        // console.log(content);
+        // console.log(quill);
+        // copy the content to clipboard
+        navigator.clipboard.writeText(content);
+        copybtn.innerHTML = 'Copied!';
+        setTimeout(() => {
+            copybtn.innerHTML = 'Copy';
+        }, 2000);
+    })
+
+    nextbtn.addEventListener('click', () => {
+        window.location.href = 'createArticle.html';
+    })
+}
+
+window.addEventListener("DOMContentLoaded", async () => {
+    setHeader();
+    setListeners();
+})
