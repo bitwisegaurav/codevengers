9a206e9db66c594cfee44f730787bd11cb2404e3
Gaurav Mishra <gauravmishra2401@gmail.com>
2024-04-17 16:19:33 +0530

    Add getArticles function and setListeners function to articles.js

--- common_files/articles.js
+++ common_files/articles.js
@@ -21,18 +21,116 @@ function setNoData(message = "No data found") {
     </div>`;
 }
 
+async function getArticles() {
+    const api = "/article/get-articles";
+
+    const data = await apiCall(api, "GET", null);
+
+    if (!data) { return null; }
+
+    return data;
+}
+
+function setData(data) {
+    if(!data) {
+        setNoData("No Articles found");
+        return;
+    }
+
+    const articles = data;
+    if(!articles || articles.length < 1) {
+        setNoData("No Articles found");
+        return;
+    }
+
+    const content = articles.map(article => {
+        const owner = article.owner;
+        const link = `article.html?id=` + article._id;
+        return `
+        <div class="article" _id="${article._id}">
+            <a href="${link}">
+                <div class="image">
+                    <img src="${article.imageURL}" alt="${article.title} image">
+                </div>
+                <div class="details">
+                    <h3 class="title">${article.title}</h3>
+                    <p class="description">${article.body}</p>
+                    <div class="user">
+                        <div class="image">
+                            <img src="${owner.avatar}" alt="${owner.username} avatar">
+                        </div>
+                        <p class="username">${owner.username}</p>
+                    </div>
+                </div>
+            </a>
+            <div class="btns">
+                <button type="button">&Cross;</button>
+            </div>
+        </div>
+        `
+    }).join("");
+
+    document.querySelector('.articles').innerHTML = content;
+}
+
+function setListeners() {
+    const articles = document.querySelectorAll('.article');
+
+    articles.forEach(article => {
+        const btn = article.querySelector('.btns button');
+
+        btn.addEventListener('click', async () => {
+            const id = article.getAttribute('_id');
+
+            // console.log(id);
+
+            const api = `/article/delete-article/${id}`;
+
+            try {
+                await apiCall(api, 'DELETE', null);
+            } catch (error) {
+                console.log("Error deleting article. Error: ", error);
+            }
+        })
+    })
+}
+
 window.addEventListener("DOMContentLoaded", async () => {
-    // setData()
     setHeader();
-    // const userCourses = await getUserCourses();
 
-    // if(!userCourses) {
-    //     window.location.href = "../login.html";
-    //     return;
-    // }
+    let sampledata = [
+        {
+            _id: "1",
+            title: "Understanding Linux filesystems: ext4 and beyond",
+            body: `The majority of modern Linux distributions default to the ext4 filesystem, just as previous Linux distributions defaulted to ext3, ext2, and—if you go back far enough—ext.
+            If you're new to Linux—or to filesystems—you might wonder what ext4 brings to the table that ext3 didn't. You might also wonder whether ext4 is still in active development at all, given the flurries of news coverage of alternate filesystems such as btrfs, xfs, and zfs.
+            Linux is known for its robustness, scalability, and reliability. It supports a wide range of hardware architectures and has a vast collection of software packages available through package managers. Additionally, Linux has a strong community of developers and users who contribute to its development and provide support.
+            <br>
+            With Linux, you have the freedom to choose from different distributions, such as Ubuntu, Fedora, and CentOS, each tailored to specific needs and preferences. These distributions come with various desktop environments, such as GNOME, KDE, and XFCE, allowing users to customize their desktop experience.
+            Linux is also widely used in the cloud computing industry, with many cloud providers offering Linux-based virtual machines and containers. It is highly regarded for its performance, security, and cost-effectiveness in cloud environments.`,
+            imageURL: "http://res.cloudinary.com/dujd69tub/image/upload/v1712076172/vylujtys5dr2ujsw3cbe.webp",
+            owner: {
+                username: "bitwisegaurav",
+                avatar: "http://res.cloudinary.com/dujd69tub/image/upload/v1709444868/sf25kufqpcrur6ccxgqn.jpg"
+            }
+        },
+        {
+            _id: "2",
+            title: "Understanding Linux filesystems: ext4 and beyond",
+            body: `The majority of modern Linux distributions default to the ext4 filesystem, just as previous Linux distributions defaulted to ext3, ext2, and—if you go back far enough—ext.
+            If you're new to Linux—or to filesystems—you might wonder what ext4 brings to the table that ext3 didn't. You might also wonder whether ext4 is still in active development at all, given the flurries of news coverage of alternate filesystems such as btrfs, xfs, and zfs.
+            Linux is known for its robustness, scalability, and reliability. It supports a wide range of hardware architectures and has a vast collection of software packages available through package managers. Additionally, Linux has a strong community of developers and users who contribute to its development and provide support.
+            <br>
+            With Linux, you have the freedom to choose from different distributions, such as Ubuntu, Fedora, and CentOS, each tailored to specific needs and preferences. These distributions come with various desktop environments, such as GNOME, KDE, and XFCE, allowing users to customize their desktop experience.
+            Linux is also widely used in the cloud computing industry, with many cloud providers offering Linux-based virtual machines and containers. It is highly regarded for its performance, security, and cost-effectiveness in cloud environments.`,
+            imageURL: "http://res.cloudinary.com/dujd69tub/image/upload/v1712076172/vylujtys5dr2ujsw3cbe.webp",
+            owner: {
+                username: "admin",
+                avatar: "http://res.cloudinary.com/dujd69tub/image/upload/v1709444868/sf25kufqpcrur6ccxgqn.jpg"
+            }
+        },
+    ]
 
-    // const courses = await getCourses(); 
-
-    // setData(courses, userCourses?.courses);
-    // setListeners();
+    setData(sampledata);
+    setListeners();
 })
