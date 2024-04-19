9f346df3244d9d6e746b07294b92867f2abfd126
Gaurav Mishra <gauravmishra2401@gmail.com>
2024-04-19 23:06:07 +0530

    Fix bug in login functionality

--- common_files/admin.js
+++ common_files/admin.js
@@ -3,6 +3,19 @@ const containerBox = document.querySelector('.container');
 const title = containerBox.querySelector('.upper .title');
 const middleBox = containerBox.querySelector('.middle');
 const bottomBox = containerBox.querySelector('#bottom');
+const moduleCount = {
+    "javascript": 62,
+    "css": 16,
+    "html": 14,
+    "discrete mathematics": 9,
+    "e commerce": 5,
+    "java": 26,
+    "mysql": 21,
+    "computer networks": 13,
+    "php": 12,
+    "pl/sql": 10,
+    "java programs": 445
+}
 
 function setDataBox(data) {
     const content = data.map(item => {
@@ -13,6 +26,12 @@ function setDataBox(data) {
     }).join('');
 
     return content;
+}
+
+function getTextFromHtml(content) {
+    const html = document.createElement('html');
+    html.innerHTML = content;
+    return html.textContent || html.innerText || "";
 }
 
 // async get header values
@@ -32,11 +51,15 @@ async function setHeader() {
 
 // database calls for getting data
 async function getUsers() {
+    const api = "/user/get-all-users";
+
+    // const data = await apiCall(api, "GET", null);
+
     const data = {
         userCount: 10,
         users: [
             {
-                image: "http://res.cloudinary.com/dujd69tub/image/upload/v1709444868/sf25kufqpcrur6ccxgqn.jpg",
+                avatar: "http://res.cloudinary.com/dujd69tub/image/upload/v1709444868/sf25kufqpcrur6ccxgqn.jpg",
                 name: "Gaurav Mishra",
                 username: "bitwisegaurav",
             }
@@ -47,35 +70,39 @@ async function getUsers() {
 }
 
 async function getCourses() {
-    const data = {
-        courseCount: 10,
-        courses: [
-            {
-                image: "http://res.cloudinary.com/dujd69tub/image/upload/v1710719793/wlzoofbw5e8wl1mbdrjt.png",
-                title: "JavaScript",
-                modulesCount: "12",
-            },
-        ]
-    };
+    const api = "/course/get-all-courses";
+
+    // const data = await apiCall(api, "GET", null);
+
+    const data = [
+        {
+            image: "http://res.cloudinary.com/dujd69tub/image/upload/v1710719793/wlzoofbw5e8wl1mbdrjt.png",
+            title: "JavaScript",
+            modulesCount: "12",
+        },
+    ];
 
     return data;
 }
 
 async function getArticles() {
-    const data = {
-        articles: [
-            {
-                image: "http://res.cloudinary.com/dujd69tub/image/upload/v1710719793/wlzoofbw5e8wl1mbdrjt.png",
-                title: "Installing Linux on an iMac",
-                description: "Many people may like the look and feel of Linux over other Desktop Operating Systems (OS). For this reason someone may want to install Linux on a Mac.",
-                user: {
-                    image: "http://res.cloudinary.com/dujd69tub/image/upload/v1709444868/sf25kufqpcrur6ccxgqn.jpg",
-                    name: "Gaurav Mishra",
-                    username: "bitwisegaurav",
-                }
-            },
-        ]
-    };
+    const api = '/article/get-articles';
+
+    // const data = await apiCall(api, "GET", null);
+
+    const data = [
+        {
+            _id: "6230dgd09fg7w0e9rui34j",
+            imageURL: "http://res.cloudinary.com/dujd69tub/image/upload/v1710719793/wlzoofbw5e8wl1mbdrjt.png",
+            title: "Installing Linux on an iMac",
+            body: "Many people may like the look and feel of Linux over other Desktop Operating Systems (OS). For this reason someone may want to install Linux on a Mac.",
+            owner: {
+                avatar: "http://res.cloudinary.com/dujd69tub/image/upload/v1709444868/sf25kufqpcrur6ccxgqn.jpg",
+                name: "Gaurav Mishra",
+                username: "bitwisegaurav",
+            }
+        },
+    ];
 
     return data;
 }
@@ -95,7 +122,7 @@ async function setUsersData() {
     const content = users.map(user => {
         return `<a href="profile.html?username=${user.username}" class="user">
         <div class="image">
-            <img src="${user.image}" alt="${user.username} image">
+            <img src="${user.avatar}" alt="${user.username} image">
         </div>
         <div class="details">
             <p class="username">${user.username}</p>
@@ -131,16 +158,27 @@ async function setCoursesData() {
         },
     ]);
 
-    const courses = data.courses;
+    const courses = data;
 
     const content = courses.map(course => {
+        const modules = moduleCount[course.title.toLowerCase()] || 0;
+        let name = course.title.toLowerCase();
+        let url = null;
+        if(courseUrls[name]) {
+            if(courseUrls[name].url) {
+                url = courseUrls[name].url;
+            } else {
+                name = courseUrls[name].name;
+            }
+        }
+
         return `<div class="course">
-        <a href="#">
+        <a href="${url ? url : `../languages/outer.html?lang=${name}`}">
             <div class="icon">
                 <img src="${course.image}" alt="${course.title} icon">
             </div>
             <p class="title">${course.title}</p>
-            <p class="modules">No. of chapters ${course.modulesCount}</p>
+            <p class="modules">No. of chapters ${modules}</p>
         </a>
         <div class="btns">
             <button type="button" class="editbtn">Edit</button>
@@ -168,27 +206,28 @@ async function setArticlesData() {
         },
     ]);
 
-    const artilces = data.articles;
+    const articles = data;
 
-    const content = artilces.map(article => {
-        return `<div class="article">
-            <a href="#">
+    const content = articles.map(article => {
+        return `<div class="article" _id="${article._id}">
+            <a href="article.html?id=${article._id}">
                 <div class="image">
-                    <img src="${article.image}" alt="${article.title} image">
+                    <img src="${article.imageURL}" alt="${article.title} image">
                 </div>
                 <div class="details">
                     <h3 class="title">${article.title}</h3>
-                    <p class="description">${article.description}</p>
+                    <p class="description">${getTextFromHtml(article.body)}</p>
                     <div class="user">
                         <div class="image">
-                            <img src="${article.user.image}" alt="${article.user.username} avatar">
+                            <img src="${article.owner.avatar}" alt="${article.owner.username} avatar">
                         </div>
-                        <p class="username">${article.user.username}</p>
+                        <p class="username">${article.owner.username}</p>
                     </div>
                 </div>
             </a>
             <div class="btns">
-                <button type="button">&Cross;</button>
+                <button type="button" class="edit">&#x270E;</button>
+                <button type="button" class="remove">&Cross;</button>
             </div>
         </div>`
     }).join('');
@@ -198,6 +237,29 @@ async function setArticlesData() {
     // remove other classes from bottomBox
     bottomBox.className = '';
     bottomBox.classList.add('articles');
+
+    const articleBoxes = document.querySelectorAll('.article');
+
+    articleBoxes.forEach(articleBox => {
+        const editbtn = articleBox.querySelector('.edit');
+        const removebtn = articleBox.querySelector('.remove');
+        const id = articleBox.getAttribute('_id');
+
+        editbtn.addEventListener('click', async () => {
+            window.location.href = `editArticle.html?id=${id}`;
+        });
+
+        removebtn.addEventListener('click', async () => {
+            const api = `/article/delete-article/${id}`;
+            
+            try {
+                await apiCall(api, "DELETE", null);
+                articleBox.parentElement.removeChild(articleBox);
+            } catch (error) {
+                console.log(error);
+            }
+        })
+    })
 }
 
 function setListeners() {
@@ -206,6 +268,9 @@ function setListeners() {
     links.forEach(link => {
         link.addEventListener('click', async (e) => {
             e.preventDefault();
+
+            localStorage.setItem('nav', link.id.toLowerCase());
+
             document.querySelector('.active').classList.remove('active');
             link.classList.add('active');
 
@@ -230,8 +295,14 @@ function setListeners() {
 
 }
 
+function opentab() {
+    const nav = localStorage.getItem('nav');
+    const link = document.querySelector(`aside ul li a#${nav ? nav : 'users'}`);
+    link.click();
+}
+
 window.addEventListener("DOMContentLoaded", async () => {
     setHeader();
     setListeners()
-    setUsersData()
+    opentab();
 })
